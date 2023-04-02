ea75ff4a8644e9bc7a0d2a18ed133ba1cf43f01c
clyton tumwesigye <herdsontisk@gmail.com>
2023-04-02 10:43:38 +0300

    this is our first proj

---
+++ NewJava.java
@@ -0,0 +1,5 @@
+public class NewJava {
+    public static void main(String[] args){
+        System.out.println("Hello this is my first project to be hosted on github version control or repository");
+    }
+}
